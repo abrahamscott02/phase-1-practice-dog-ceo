1ebdef49454aa4faccd1d1730a20660425f08b84
Abraham Scott <abrahamscott02@gmail.com>
2023-12-11 13:34:05 -0700

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    const breedDropdown = document.getElementById("breed-dropdown");
+  
+    fetch(imgUrl)
+      .then(response => response.json())
+      .then(data => {
+        const imageContainer = document.getElementById("dog-image-container");
+  
+        data.message.forEach(imageUrl => {
+          const img = document.createElement("img");
+          img.src = imageUrl;
+          imageContainer.appendChild(img);
+        });
+      });
+  
+    fetch(breedUrl)
+      .then(response => response.json())
+      .then(data => {
+        const breedList = document.getElementById("dog-breeds");
+        const breeds = data.message;
+  
+        for (const breed in breeds) {
+          const li = document.createElement("li");
+          li.textContent = breed;
+          breedList.appendChild(li);
+        }
+  
+        breedDropdown.addEventListener("change", function() {
+          const selectedLetter = breedDropdown.value;
+  
+          // Clear the previous breed list
+          breedList.innerHTML = "";
+  
+          for (const breed in breeds) {
+            if (breed.startsWith(selectedLetter)) {
+                const li = document.createElement('li')
+                li.textContent = breed 
+                breedList.appendChild(li)
+            }
+          }
+        });
+      });
+  });
